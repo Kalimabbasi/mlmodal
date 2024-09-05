eeed74dd91604eb28b7f3e2a61910a96b50211d2
Kalimabbasi <bahriafoundation2@gmail.com>
2024-09-05 11:42:38 +0000

    Second commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 import pandas as pd
 # import pickle
 import joblib
